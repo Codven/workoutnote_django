ca8ac13a827e90b14652e1d9106178e12a4122fd
Nematjon Narziev <nematjonnarziev@MacBook-Pro.local>
2021-06-19 15:10:26 +0500

    Implement login and register pages

--- workoutnote_django/urls.py
+++ workoutnote_django/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('calculators/', views.handle_calculators, name='calculators'),
     path('strength-standards/', views.handle_strength_standards, name='strength-standards'),
     path('training-log-tutorial/', views.handle_training_log_tutorial, name='training-log-tutorial'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_register, name='login'),
+    path('register/', views.login_register, name='register'),
 ]
--- workoutnote_django/views.py
+++ workoutnote_django/views.py
@@ -17,5 +17,26 @@ def handle_training_log_tutorial(request):
     return render(request=request, template_name='training-log-tutorial.html')
 
 
-def login(request):
-    return render(request=request, template_name='login.html')
+from django.template.response import TemplateResponse
+
+
+def login_register(request):
+    data = {
+        'title': 'Log In',
+        'subtitle': 'Need an account?',
+        'subtitle_link': '/register',
+        'subtitle_link_text': 'Register',
+        'button_text': 'Log In',
+        'forgot_password_link': '/forgot-password',
+        'forgot_password_text': 'Forgot password?'
+    }
+    if request.path == '/register/':
+        data['title'] = 'Register'
+        data['subtitle'] = 'Already have an account?'
+        data['subtitle_link'] = '/login'
+        data['subtitle_link_text'] = 'Log In'
+        data['button_text'] = 'Register'
+        data['forgot_password_link'] = ''
+        data['forgot_password_text'] = ''
+
+    return TemplateResponse(request=request, template='login.html', context=data)

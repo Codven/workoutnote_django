7a4ba4f2c1d221d98b196d70b6d9acc2472242d2
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-06-19 18:52:53 +0900

    bg changed & one rep max page added

--- workoutnote_django/urls.py
+++ workoutnote_django/urls.py
@@ -21,7 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.handle_index, name='index'),
     path('calculators/', views.handle_calculators, name='calculators'),
-    path('strength-standards/', views.handle_strength_standards, name='strength-standards'),
-    path('training-log-tutorial/', views.handle_training_log_tutorial, name='training-log-tutorial'),
-    path('login/', views.login, name='login'),
+    path('strength-standards/', views.handle_strength_standards, name='strength standards'),
+    path('training-log-tutorial/', views.handle_training_log_tutorial, name='training log tutorial'),
+    path('one-rep-max-calculator/', views.handle_one_rep_max_calculator, name='one rep max calculator'),
+
+    path('login/', views.handle_login, name='login'),
 ]
--- workoutnote_django/views.py
+++ workoutnote_django/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render
 
 
 def handle_index(request):
-    return render(request=request, template_name='index.html')
+    return render(request=request, template_name='index/index.html')
 
 
 def handle_calculators(request):
-    return render(request=request, template_name='calculators.html')
+    return render(request=request, template_name='index/calculators.html')
 
 
 def handle_strength_standards(request):
-    return render(request=request, template_name='strength_standards.html')
+    return render(request=request, template_name='index/strength standards.html')
 
 
 def handle_training_log_tutorial(request):
-    return render(request=request, template_name='training-log-tutorial.html')
+    return render(request=request, template_name='index/training log tutorial.html')
 
 
-def login(request):
-    return render(request=request, template_name='login.html')
+def handle_login(request):
+    return render(request=request, template_name='index/sub html authentication.html')
+
+
+def handle_one_rep_max_calculator(request):
+    return render(request=request, template_name='index/one rep max calculator.html')

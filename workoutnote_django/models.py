3fd5fe5e3d3fdc5c124116f86299da6dec753a3b
Nematjon <backend@palligroup.com>
2021-06-22 13:14:32 +0500

    Add Preferences model to django-admin, add choices to fields of some models

--- workoutnote_django/models.py
+++ workoutnote_django/models.py
@@ -7,26 +7,40 @@ class Preferences(models.Model):
         MALE = "MALE"
         FEMALE = "FEMALE"
         ALL = [MALE, FEMALE]
+        CHOICES = (
+            (MALE, 'Male'),
+            (FEMALE, 'Female')
+        )
 
     class ProfileSharing:
         SHARED_WITH_PUBLIC = "SHARED_WITH_PUBLIC"
         SHARED_WITH_USERS = "SHARED_WITH_USERS"
         PRIVATE = "PRIVATE"
         ALL = [SHARED_WITH_PUBLIC, SHARED_WITH_USERS, PRIVATE]
+        CHOICES = (
+            (SHARED_WITH_PUBLIC, 'Shared with people'),
+            (SHARED_WITH_USERS, 'Shared with users'),
+            (PRIVATE, 'Private'),
+        )
 
     class MeasurementUnit:
         KILOGRAMS = "KG"
         POUNDS = "LB"
         STONE = "ST"
         ALL = [KILOGRAMS, POUNDS, STONE]
+        CHOICES = (
+            (KILOGRAMS, 'kg'),
+            (POUNDS, 'lb'),
+            (STONE, 'st'),
+        )
 
     user = models.OneToOneField(to=django_User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=128, default=None, null=True)
-    gender = models.CharField(max_length=16, default=Gender.MALE)
-    date_of_birth = models.DateField(default=None)
+    gender = models.CharField(max_length=16, default=Gender.MALE, choices=Gender.CHOICES)
+    date_of_birth = models.DateField(default=None, null=True)
     height = models.FloatField(default=None, null=True)
-    profile_sharing = models.CharField(max_length=128, default=ProfileSharing.PRIVATE)
-    unit_of_measure = models.CharField(max_length=128, default=MeasurementUnit.KILOGRAMS)
+    profile_sharing = models.CharField(max_length=128, default=ProfileSharing.PRIVATE, choices=ProfileSharing.CHOICES)
+    unit_of_measure = models.CharField(max_length=128, default=MeasurementUnit.KILOGRAMS, choices=MeasurementUnit.CHOICES)
 
     def gender_str(self):
         return self.gender.__str__()

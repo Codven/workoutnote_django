9a4c3ef85eb2f3638728b845ed99388415d29d61
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-06-27 04:20:36 +0900

    strengthlevel removed

--- workoutnote_django/urls.py
+++ workoutnote_django/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [

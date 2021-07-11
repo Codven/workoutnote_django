3918bbacaf44b4bb474d12d5504d1745c5107414
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-07-12 03:44:22 +0900

    db changed to rds

--- workoutnote_django/settings.py
+++ workoutnote_django/settings.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = 'workoutnote_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'deltoid-db',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'postgres',
-        # 'HOST': 'deltoid-db.cssqpqimlbjy.ap-northeast-2.rds.amazonaws.com',
-        # 'PORT': '5432'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deltoid-db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'deltoid-db.cqhzh9o0wcmk.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
